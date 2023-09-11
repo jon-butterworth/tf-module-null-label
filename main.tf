b768f2697c4725f21d354f957055ab6ce9619218
jon <jon@jonbut.com>
2023-09-11 14:36:01 +0100

    f

--- main.tf
+++ main.tf
@@ -1,6 +1,10 @@
 locals {
+
   defaults = {
-    label_order         = ["company", "region", "stage", "name", "attributes"]
+    # The `tenant` label was introduced in v0.25.0. To preserve backward compatibility, or, really, to ensure
+    # that people using the `tenant` label are alerted that it was not previously supported if they try to
+    # use it in an older version, it is not included by default.
+    label_order         = ["namespace", "environment", "stage", "name", "attributes"]
     regex_replace_chars = "/[^-a-zA-Z0-9]/"
     delimiter           = "-"
     replacement         = ""
@@ -8,22 +12,48 @@ locals {
     id_hash_length      = 5
     label_key_case      = "title"
     label_value_case    = "lower"
+
+    # The default value of labels_as_tags cannot be included in this
+    # defaults` map because it creates a circular dependency
   }
 
   default_labels_as_tags = keys(local.tags_context)
+  # Unlike other inputs, the first setting of `labels_as_tags` cannot be later overridden. However,
+  # we still have to pass the `input` map as the context to the next module. So we need to distinguish
+  # between the first setting of var.labels_as_tags == null as meaning set the default and do not change
+  # it later, versus later settings of var.labels_as_tags that should be ignored. So, we make the
+  # default value in context be "unset", meaning it can be changed, but when it is unset and
+  # var.labels_as_tags is null, we change it to "default". Once it is set to "default" we will
+  # not allow it to be changed again, but of course we have to detect "default" and replace it
+  # with local.default_labels_as_tags when we go to use it.
+  #
+  # We do not want to use null as default or unset, because Terraform has issues with
+  # the value of an object field being null in some places and [] in others.
+  # We do not want to use [] as default or unset because that is actually a valid setting
+  # that we want to have override the default.
+  #
+  # To determine whether that context.labels_as_tags is not set,
+  # we have to cover 2 cases: 1) context does not have a labels_as_tags key, 2) it is present and set to ["unset"]
   context_labels_as_tags_is_unset = try(contains(var.context.labels_as_tags, "unset"), true)
 
+  # So far, we have decided not to allow overriding replacement or id_hash_length
   replacement    = local.defaults.replacement
   id_hash_length = local.defaults.id_hash_length
 
+  # The values provided by variables supersede the values inherited from the context object,
+  # except for tags and attributes which are merged.
   input = {
+    # It would be nice to use coalesce here, but we cannot, because it
+    # is an error for all the arguments to coalesce to be empty.
     enabled   = var.enabled == null ? var.context.enabled : var.enabled
-    company = var.company == null ? var.context.company : var.company
+    namespace = var.namespace == null ? var.context.namespace : var.namespace
+    # tenant was introduced in v0.25.0, prior context versions do not have it
     tenant      = var.tenant == null ? lookup(var.context, "tenant", null) : var.tenant
-    region = var.region == null ? var.context.region : var.region
+    environment = var.environment == null ? var.context.environment : var.environment
     stage       = var.stage == null ? var.context.stage : var.stage
     name        = var.name == null ? var.context.name : var.name
     delimiter   = var.delimiter == null ? var.context.delimiter : var.delimiter
+    # modules tack on attributes (passed by var) to the end of the list (passed by context)
     attributes = compact(distinct(concat(coalesce(var.context.attributes, []), coalesce(var.attributes, []))))
     tags       = merge(var.context.tags, var.tags)
 
@@ -41,7 +71,9 @@ locals {
 
   enabled             = local.input.enabled
   regex_replace_chars = coalesce(local.input.regex_replace_chars, local.defaults.regex_replace_chars)
-  string_label_names = ["company", "tenant", "region", "stage", "name"]
+
+  # string_label_names are names of inputs that are strings (not list of strings) used as labels
+  string_label_names = ["namespace", "tenant", "environment", "stage", "name"]
   normalized_labels = { for k in local.string_label_names : k =>
     local.input[k] == null ? "" : replace(local.input[k], local.regex_replace_chars, local.replacement)
   }
@@ -58,9 +90,9 @@ locals {
     local.label_value_case == "upper" ? upper(v) : lower(v))
   ]))
 
-  company   = local.formatted_labels["company"]
+  namespace   = local.formatted_labels["namespace"]
   tenant      = local.formatted_labels["tenant"]
-  region = local.formatted_labels["region"]
+  environment = local.formatted_labels["environment"]
   stage       = local.formatted_labels["stage"]
   name        = local.formatted_labels["name"]
 
@@ -69,7 +101,11 @@ locals {
   id_length_limit  = local.input.id_length_limit == null ? local.defaults.id_length_limit : local.input.id_length_limit
   label_key_case   = local.input.label_key_case == null ? local.defaults.label_key_case : local.input.label_key_case
   label_value_case = local.input.label_value_case == null ? local.defaults.label_value_case : local.input.label_value_case
+
+  # labels_as_tags is an exception to the rule that input vars override context values (see above)
   labels_as_tags = contains(local.input.labels_as_tags, "default") ? local.default_labels_as_tags : local.input.labels_as_tags
+
+  # Just for standardization and completeness
   descriptor_formats = local.input.descriptor_formats
 
   additional_tag_map = merge(var.context.additional_tag_map, var.additional_tag_map)
@@ -85,12 +121,13 @@ locals {
   ])
 
   tags_context = {
-    company   = local.company
+    namespace   = local.namespace
     tenant      = local.tenant
-    region = local.region
+    environment = local.environment
     stage       = local.stage
-    name        = local.id
-    attributes  = local.id_context.attributes
+    # For AWS we need `Name` to be disambiguated since it has a special meaning
+    name       = local.id
+    attributes = local.id_context.attributes
   }
 
   generated_tags = {
@@ -101,9 +138,9 @@ locals {
   }
 
   id_context = {
-    company   = local.company
+    namespace   = local.namespace
     tenant      = local.tenant
-    region = local.region
+    environment = local.environment
     stage       = local.stage
     name        = local.name
     attributes  = join(local.delimiter, local.attributes)
@@ -112,23 +149,29 @@ locals {
   labels = [for l in local.label_order : local.id_context[l] if length(local.id_context[l]) > 0]
 
   id_full = join(local.delimiter, local.labels)
-
+  # Create a truncated ID if needed
   delimiter_length = length(local.delimiter)
-
+  # Calculate length of normal part of ID, leaving room for delimiter and hash
   id_truncated_length_limit = local.id_length_limit - (local.id_hash_length + local.delimiter_length)
-
+  # Truncate the ID and ensure a single (not double) trailing delimiter
   id_truncated = local.id_truncated_length_limit <= 0 ? "" : "${trimsuffix(substr(local.id_full, 0, local.id_truncated_length_limit), local.delimiter)}${local.delimiter}"
+  # Support usages that disallow numeric characters. Would prefer tr 0-9 q-z but Terraform does not support it.
+  # Probably would have been better to take the hash of only the characters being removed,
+  # so identical removed strings would produce identical hashes, but it is not worth breaking existing IDs for.
   id_hash_plus = "${md5(local.id_full)}qrstuvwxyz"
   id_hash_case = local.label_value_case == "title" ? title(local.id_hash_plus) : local.label_value_case == "upper" ? upper(local.id_hash_plus) : local.label_value_case == "lower" ? lower(local.id_hash_plus) : local.id_hash_plus
   id_hash      = replace(local.id_hash_case, local.regex_replace_chars, local.replacement)
+  # Create the short ID by adding a hash to the end of the truncated ID
   id_short = substr("${local.id_truncated}${local.id_hash}", 0, local.id_length_limit)
   id       = local.id_length_limit != 0 && length(local.id_full) > local.id_length_limit ? local.id_short : local.id_full
 
+
+  # Context of this label to pass to other label modules
   output_context = {
     enabled             = local.enabled
-    company           = local.company
+    namespace           = local.namespace
     tenant              = local.tenant
-    region         = local.region
+    environment         = local.environment
     stage               = local.stage
     name                = local.name
     delimiter           = local.delimiter
